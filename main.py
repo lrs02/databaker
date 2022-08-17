f205f11dd450037406d26cc49ccbbde9e8bdd056
lrs <liruishan@163.com>
2022-08-17 13:59:45 +0800

    add main.py first

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# 按间距中的绿色按钮以运行脚本。
+if __name__ == '__main__':
+    print('PyCharm')
+    from decimal import Decimal
+    a = Decimal("10")-Decimal("5")
+    print(a >2)
+    print(type("|".join(["1","2"])+'\n'))
+
+# first
+
